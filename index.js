aaf0c27f6fccb138d79da8b5fd2f713a0ec799dc
Cynthia <cynthia.abuya@student.moringaschool.com>
2025-03-23 16:50:04 +0300

    First commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let randomNum = Math.floor(Math.random()* 100) + 1;
+let random = randomNum;
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 12, 3, 14, 20);
+console.log(max);
